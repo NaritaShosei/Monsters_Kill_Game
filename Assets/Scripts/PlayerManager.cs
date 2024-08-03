977f5f93c2af67a771898015f36600f02e1a0b26
NaritaShosei <onigirikun7171@gmail.com>
2024-08-03 15:20:56 +0900

    攻撃と防御

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -8,11 +5,15 @@ public class PlayerManager : MonoBehaviour
 {
     [SerializeField] float _moveSpeed;
     [SerializeField] float _jumpPower;
+    [SerializeField] float _life;
     Rigidbody2D _rb2d;
     float _hMove;
     Animator _anim;
     SpriteRenderer _sprite;
     bool _isGround;
+    bool _isAttack;
+    bool _isBlock;
+    float _attackTimer;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +25,13 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Move();
-        Jump();
+        if (!_isBlock)
+        {
+            Move();
+            Jump();
+            Attack();
+        }
+        Block();
     }
     void Move()
     {
@@ -35,32 +41,73 @@ public class PlayerManager : MonoBehaviour
     void Jump()
     {
         Vector2 velocity = _rb2d.velocity;
-        if (Input.GetKey(KeyCode.Space) && _isGround)
+        if (Input.GetKeyDown(KeyCode.Space) && _isGround)
         {
             _isGround = false;
             velocity.y = _jumpPower;
         }
         _rb2d.velocity = velocity;
     }
+    void Attack()
+    {
+        _attackTimer += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isAttack = true;
+        }
+        else if (_attackTimer > 0.25)
+        {
+            _isAttack = false;
+        }
+    }
+    void Block()
+    {
+        if (_isBlock)
+        {
+            _hMove = Input.GetAxisRaw("Horizontal");
+        }
+        if (Input.GetMouseButton(1))
+        {
+            _isBlock = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            _isBlock = false;
+        }
+    }
+    void Life(float plusLife, float minusLife)
+    {
+        var life = plusLife + -minusLife;
+        _life += life;
+    }
+
     private void LateUpdate()
     {
         if (_hMove != 0)
         {
-            _sprite.flipX = (_hMove < 0);
+            _sprite.flipX = _hMove < 0;
         }
         if (_anim)
         {
             _anim.SetFloat("MoveX", Mathf.Abs(_rb2d.velocity.x));
             _anim.SetBool("IsGround", _isGround);
-            _anim.SetFloat("MoveY",_rb2d.velocity.y);
+            _anim.SetFloat("MoveY", _rb2d.velocity.y);
+            _anim.SetBool("IsAttack", _isAttack);
+            _anim.SetBool("IsBlock", _isBlock);
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        _isGround = true;
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGround = true;
+        }
     }
     private void OnCollisionExit2D(Collision2D collision)
     {
-        _isGround = false;
+        if (collision.gameObject.tag == "Ground")
+        {
+            _isGround = false;
+        }
     }
 }
